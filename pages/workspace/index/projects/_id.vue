34fcbc613931824ded69ee2cf47b82277f6107ca
h-sargsyan <hovhannessargsyan100@gmail.com>
2022-06-22 09:49:03 +0400

    Dashboard Desk page UI and functionality without API

---
+++ pages/workspace/index/projects/_id.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectsId',
+}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ pages/workspace/index/tasks/_id.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'TasksId',
+}
+</script>
+
+<style scoped lang="scss">
+</style>
